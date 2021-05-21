8d3e401d091d60fb2cacf014d7b804e1b44bb590
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-21 17:30:13 +0530

    showing custom component inside of a component, passing at as a props

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetails from './CommentDetails'
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
        <div className="ui container comments">
+           
+           
+           <ApprovalCard>
            <CommentDetails author="Sam" timeAgo="Today at 4:45PM" avatar={faker.image.avatar()}/>
+           </ApprovalCard>
+           <ApprovalCard>
            <CommentDetails author="Alex" timeAgo="Monday at 5:25AM" avatar={faker.image.avatar()}/>
+           </ApprovalCard>
+           <ApprovalCard>
            <CommentDetails author="Jane" timeAgo="Yesterday at 8:45PM" avatar={faker.image.avatar()}/>
+           </ApprovalCard>
+
        </div> 
     )
 }
